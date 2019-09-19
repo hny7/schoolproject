3b1e02c251842b115de8ec78d7671d1adf5e53ed
Pooja Sharma <poojasharma5501@gmail.com>
2019-09-19 17:31:07 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Manjari&display=swap" rel="stylesheet">
+</head>
+<body>
+	<section class="loginpage">
+		<div class="container">
+			<div class="logimg">
+			 	<p class="logtext">Mapple High School</p>
+			</div>
+			<div class="logform">
+				<h4 class="lo">Login</h4>
+				<form action="" method="POST">
+					<label class="email">Email*</label>  <br><input class="email" type="email" name="email" placeholder="Email"><br></br>
+					<label class="password">Password*</label> <br><input class="password" type="password" name="password" placeholder="Password"><br></br>
+					<button class="login">Login</button>
+				</form>
+				<p class="back"><a href=""><b style="color: black">Back To Website</b></a> </p>
+				<p class="forget"><a href=""><b style="color: black">Forget Password?</b> </a></p>	
+			</div>
+		</div>
+		<p style="text-align: center;margin-top: 25px;">Login as</p>
+		<button class="Admin">Admin</button>
+		<button class="Teacher">Teacher</button>
+		<button class="Parent">Parent</button>
+		<button class="Student">Student</button>
+		<button class="Accountant">Accountant</button>
+		<button class="Librarian">Librarian</button>
+	</section>
+</body>
+</html>
